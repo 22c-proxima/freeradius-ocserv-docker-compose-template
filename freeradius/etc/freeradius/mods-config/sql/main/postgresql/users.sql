fc495a90cacf838d9e7bfba122532f06e0724393
Владимир Шомин <22c.proxima@gmail.com>
2025-08-27 02:53:53 +0200

    Лимиты подцепились

--- freeradius/etc/freeradius/mods-config/sql/main/postgresql/users.sql
+++ freeradius/etc/freeradius/mods-config/sql/main/postgresql/users.sql
@@ -1,7 +1,22 @@
--- Юзер по умолчанию
-INSERT INTO radcheck (username, attribute, op, value) VALUES ('demo','Cleartext-Password',':=','123');
-INSERT INTO radcheck (username, attribute, op, value) VALUES ('demo','Simultaneous-Use',':=','2');
+-- Группа advanced, каждому её члену позволено две сессии максимум
+INSERT INTO radgroupcheck (groupname, attribute, op, value)
+VALUES ('advanced','Simultaneous-Use',':=','2');
 
--- Юзер для теста
-INSERT INTO radcheck (username, attribute, op, value) VALUES ('test','Cleartext-Password',':=','test');
-INSERT INTO radcheck (username, attribute, op, value) VALUES ('test','Simultaneous-Use',':=','2');
+-- ... и скорость: 50 килоБАЙТ/с в обе стороны
+INSERT INTO radgroupreply (groupname, attribute, op, value)
+VALUES ('advanced','RP-Upstream-Speed-Limit',':=','50'),
+       ('advanced','RP-Downstream-Speed-Limit',':=','50');
+
+-- Юзер по умолчанию, принадлежит группе advanced
+INSERT INTO radcheck (username, attribute, op, value)
+VALUES ('demo','Cleartext-Password',':=','123');
+INSERT INTO radusergroup (username, groupname, priority)
+VALUES ('demo','advanced',1);
+
+-- Юзер для теста, он имеет собственные ограничения: 1 сессия и 100 килоБАЙТ/с
+INSERT INTO radcheck (username, attribute, op, value)
+VALUES ('test','Cleartext-Password',':=','test'),
+       ('test','Simultaneous-Use',':=','1');
+INSERT INTO radreply (username, attribute, op, value)
+VALUES ('test','RP-Upstream-Speed-Limit',':=','100'),
+       ('test','RP-Downstream-Speed-Limit',':=','100');
